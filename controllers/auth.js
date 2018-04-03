bf218bef53ed845ac2b58305b5a0bee64d03037b
GiannisTsim <giannis.tsim.89@gmail.com>
2018-04-03 18:29:34 +0300

    authentication-authorization middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const generateJWT = (email, username, role ) => {
+const generateJWT = (email, username, role) => {
   return jwt.sign(
     {
       email,
@@ -20,31 +20,89 @@ const toAuthJSON = ({ email, username, role }) => {
   };
 };
 
-
-const handleSignin = (db, bcrypt) => (req,res) => {
+const handleSignin = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body.credentials;
-  
+
   // TODO: add more checks
   if (!email || !password) {
-    return res.status(400)
-      .json({ errors: { global: "invalid Credentials 3"}});
+    return res
+      .status(400)
+      .json({ errors: { global: "invalid Credentials 3" } });
   }
-  db.select('*').from('users')
-    .where({ email }).first()
-    .then(data => {     
-      if(bcrypt.compareSync(password, data.password)) {
+  db
+    .select("*")
+    .from("users")
+    .where({ email })
+    .first()
+    .then(data => {
+      if (bcrypt.compareSync(password, data.password)) {
         return res.json({ user: toAuthJSON(data) });
-      }
-      else {
+      } else {
         // error for invalid password
-        res.status(400).json({ errors: { global: "invalid Credentials (invalid password)"}});
+        res.status(400).json({
+          errors: {
+            global: "invalid Credentials (invalid password)"
+          }
+        });
       }
     })
     // error for invalid email
-    .catch(err => res.status(400).json({ errors: { global: "invalid Credentials (email does not exist)"}}))
+    .catch(err =>
+      res.status(400).json({
+        errors: {
+          global: "invalid Credentials (email does not exist)"
+        }
+      })
+    );
+};
+
+const authenticate = (req, res, next) => {
+  const header = req.headers.authorization;
+  let token;
+  // header form:
+  //"Bearer sometokenhere"
+  if (header) token = header.split(" ")[1];
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).json({
+          errors: {
+            global: " Invalid token. Cannot authenticate user"
+          }
+        });
+      } else {
+        req.user = {
+          email: decoded.email,
+          role: decoded.role,
+          username: decoded.username
+        };
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({
+      errors: {
+        global: " No token provided. Cannot authenticate user"
+      }
+    });
+  }
+};
+
+const checkAuthorization = authorizedRoles => (req, res, next) => {
+  if (authorizedRoles.includes(req.user.role)) {
+    next();
+  } else {
+    res.status(403).json({
+      errors: {
+        global: "Unauthorized user"
+      }
+    });
+  }
 };
 
 module.exports = {
   handleSignin,
-  toAuthJSON
+  toAuthJSON,
+  authenticate
 };
--- controllers/manager.js
+++ controllers/manager.js
@@ -1,7 +1,7 @@
-const auth = require('./auth');
+const auth = require("./auth");
 
-const handleRegister = (db, bcrypt) => (req,res) => {
-  const { 
+const handleRegister = (db, bcrypt) => (req, res) => {
+  const {
     email,
     username,
     password,
@@ -12,62 +12,64 @@ const handleRegister = (db, bcrypt) => (req,res) => {
     city,
     street,
     number
-    } = req.body.data;
+  } = req.body.data;
 
   // TODO: add more checks
   if (!email || !username || !password || !company_name) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json("incorrect form submission");
   }
 
-  return db.transaction(trx => {
-    return db      
-      .insert({
-        email,
-        username,
-        password: bcrypt.hashSync(password, 10),
-        first_name,
-        last_name,
-        role: 'manager'
-      })
-      .into('users')
-      .transacting(trx)
-      .then(() => {
-        return db
+  return db
+    .transaction(trx => {
+      return db
         .insert({
-          company_name,
-          country,
-          city,
-          street,
-          number
+          email,
+          username,
+          password: bcrypt.hashSync(password, 10),
+          first_name,
+          last_name,
+          role: "manager"
         })
-        .into('companies')
+        .into("users")
         .transacting(trx)
-        .returning('company_id')
-        .then((ids) => { 
+        .then(() => {
           return db
             .insert({
-              email,
-              company_id: ids[0]
+              company_name,
+              country,
+              city,
+              street,
+              number
             })
-            .into('managers')
+            .into("companies")
             .transacting(trx)
+            .returning("company_id")
+            .then(ids => {
+              return db
+                .insert({
+                  email,
+                  company_id: ids[0]
+                })
+                .into("managers")
+                .transacting(trx);
+            });
         })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-  })
-  .then(() => 
-    // transaction suceeded, database tables changed
-    res.json({ user: auth.toAuthJSON({ email, username, role:'manager' }) }))
-  .catch(err => 
-    // transanction failed, no database changes
-    {
-      console.log(err);
-      res.status(400).json({ errors: { global: "unable to register"} })
+        .then(trx.commit)
+        .catch(trx.rollback);
     })
+    .then(() =>
+      // transaction suceeded, database tables changed
+      res.json({ user: auth.toAuthJSON({ email, username, role: "manager" }) })
+    )
+    .catch(err =>
+      // transanction failed, no database changes
+      {
+        console.log(err);
+        res.status(400).json({ errors: { global: "unable to register" } });
+      }
+    );
 };
 
-
 module.exports = {
-  handleRegister,
-}
+  handleRegister
+};
